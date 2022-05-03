4392da3e53441b3fa12054a760fb53c47571d8d0
Hoang Hoa <kieuhoa256@gmail.com>
2022-05-03 12:52:44 +0700

    Update Home Component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -19,8 +19,11 @@ const Home = () => {
           building response full-stack web applications.
         </p>
         <div>
-          <button className="text-white border-2 px-6 py3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600">
-            View Work <HiArrowNarrowRight />
+          <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600">
+            View Work
+            <span className="group-hover:rotate-90 duration-300">
+              <HiArrowNarrowRight className="ml-3" />
+            </span>
           </button>
         </div>
       </div>
