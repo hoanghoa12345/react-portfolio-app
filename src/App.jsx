cb2add0f72c13684d76b6bf30ed7a19ebcd4c180
Hoang Hoa <kieuhoa256@gmail.com>
2022-05-03 13:17:40 +0700

    Add about component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import About from "./components/About";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 
@@ -9,6 +10,7 @@ const App = () => (
   <div>
     <Navbar />
     <Home />
+    <About />
   </div>
 );
 ReactDOM.render(<App />, document.getElementById("app"));
