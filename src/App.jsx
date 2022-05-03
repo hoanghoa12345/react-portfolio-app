982fc034b9fbb7e70d1f604f60dcb64b56536294
Hoang Hoa <kieuhoa256@gmail.com>
2022-05-03 14:05:52 +0700

    Add Skills component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import About from "./components/About";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
+import Skills from "./components/Skills";
 
 import "./index.scss";
 
@@ -11,6 +12,7 @@ const App = () => (
     <Navbar />
     <Home />
     <About />
+    <Skills />
   </div>
 );
 ReactDOM.render(<App />, document.getElementById("app"));
